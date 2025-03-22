0e1669e19d9f8e78ea568f2347955882efdb59a5
Sachinnimap <sachin.ram@nimapinfotech.com>
2025-03-22 19:13:31 +0530

    Add files via upload

---
+++ march/22_03_2025.mjs
@@ -0,0 +1,44 @@
+
+// let newArr = ['sih','eaa','rssjd'];
+
+// console.log(newArr.sort())
+// let userName;
+// if (userName) {
+//     console.log("User exists!"); 
+// }
+
+
+// var firstName;
+// firstName = 'sachin';
+
+// (function latestFunc (){
+//     var secondName;
+//     secondName = "raaaj"
+// })()
+
+// console.log(secondName)
+
+
+// class Women{
+//     constructor(name,email){
+//         this.name = name,
+//         this.email = email
+//     }
+
+//      getData(params) {
+//         console.log(`my name is ${this.name} and my emeil is ${this.email}`);
+//     }
+// }
+
+//  let newV = new Women('sachin','sahcin@gmail.com');
+//  newV.getData()
+
+//  console.log("Start");
+
+//  await new Promise(resolve => setTimeout(resolve, 2000))
+//   .then(() => {
+//     console.log("Inside Timeout");
+//     console.log("End");
+//   });
+
+//   console.log("last")
